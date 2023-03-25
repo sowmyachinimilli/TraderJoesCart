7f20ca28f0cec846c56538e5bd140b445697e191
sowmyachinimilli <chinimillisowmya@gmail.com>
2023-03-25 16:39:44 -0400

    added card layout for products

---
+++ frontend/src/products.js
@@ -0,0 +1,69 @@
+const products = [
+    {
+        id:'1',
+        name: 'Korean Beafless' ,
+        image: '/images/cardimg1.jpg' ,
+        description:   
+        'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience. Built-in microphone allows you to take calls while working',
+        brand: 'Apple',
+        category: 'Electronics',
+        price: 89.99,
+        countInStock: 3, 
+        rating: 4.5, 
+        numReviews: 4,
+    },
+    {
+        id:'2',
+        name: 'Black Tea' ,
+        image: '/images/cardimg2.jpg' ,
+        description:   
+        'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience. Built-in microphone allows you to take calls while working',
+        brand: 'Apple',
+        category: 'Electronics',
+        price: 89.99,
+        countInStock: 3, 
+        rating: 4.5, 
+        numReviews: 4,
+    },
+    {
+        id:'3',
+        name: 'Black Tea' ,
+        image: '/images/cardimg3.jpg' ,
+        description:   
+        'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience. Built-in microphone allows you to take calls while working',
+        brand: 'Apple',
+        category: 'Electronics',
+        price: 89.99,
+        countInStock: 3, 
+        rating: 4.5, 
+        numReviews: 4,
+    },
+    {
+        id:'4',
+        name: 'Black Tea' ,
+        image: '/images/cardimg4.jpg' ,
+        description:   
+        'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience. Built-in microphone allows you to take calls while working',
+        brand: 'Apple',
+        category: 'Electronics',
+        price: 89.99,
+        countInStock: 3, 
+        rating: 4.5, 
+        numReviews: 4,
+    },
+    {
+        id:'5',
+        name: 'Black Tea' ,
+        image: '/images/cardimg5.jpg' ,
+        description:   
+        'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience. Built-in microphone allows you to take calls while working',
+        brand: 'Apple',
+        category: 'Electronics',
+        price: 89.99,
+        countInStock: 3, 
+        rating: 4.5, 
+        numReviews: 4,
+    }
+]
+
+export default products;
