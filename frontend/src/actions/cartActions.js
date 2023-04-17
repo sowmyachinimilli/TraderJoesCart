8dddffc3ac198540d1184b571f2e27e96e47ef40
Aditi chikkali <chikkali.a@northeastern.edu>
2023-04-17 18:37:33 -0400

    delete cart functionality

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { CART_ADD_ITEM } from '../constants/cartConstants';
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants';
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
   const { data } = await axios.get(`/api/products/${id}`);
@@ -17,4 +17,18 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
   });
 
   localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+};
+
+export const removeFromCart = (id) => (dispatch, getState) => {
+  dispatch({
+    type: CART_REMOVE_ITEM,
+    payload: id,
+  });
+
+  setTimeout(() => {
+    localStorage.setItem(
+      'cartItems',
+      JSON.stringify(getState().cart.cartItems)
+    );
+  }, 10);
 };
--- frontend/src/pages/ProductPage.js
+++ frontend/src/pages/ProductPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import history from '../history.js';
+//import history from '../history.js';
 
 import { Link, useParams, useNavigate } from 'react-router-dom';
 import {
