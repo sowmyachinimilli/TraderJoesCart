17a8ec858cb5fc565e7c309c706d17f68607578b
sowmyachinimilli <chinimillisowmya@gmail.com>
2023-04-18 02:53:58 -0400

    updated user authentication

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -3,7 +3,7 @@ import User from '../models/userModel.js';
 import generateToken from '../utils/generateToken.js';
 
 //@desc    Auth user & get a token
-//@route   GET /api/user/login
+//@route   POST /api/user/login
 //@access  public
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
@@ -16,6 +16,7 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
+      // token: null
       token: generateToken(user._id),
     });
   } else {
@@ -24,11 +25,58 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+//@desc    Register a new user 
+//@route   POST /api/user/login
+//@access  public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if(userExists){
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  })
+
+  if(user){
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    })
+  }
+  else{
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
+});
+
 //@desc    get user profile
 //@route   GET /api/users/profile
 //@access  private
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send('Success');
+  const user = await User.findById(req.user._id)
+  
+  if(user){
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  }
+  else{
+    res.status(401)
+    throw new Error('User not found')
+  }
 });
 
-export { authUser, getUserProfile };
+export { authUser, registerUser, getUserProfile };
