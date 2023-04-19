a71b4d6044977e7d3f473b5c059b11d3a7c2f1f4
sowmyachinimilli <chinimillisowmya@gmail.com>
2023-04-19 02:59:30 -0400

    updated order backend

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { addOrderItems } from '../controllers/orderController.js';
-import { authUser, registerUser, getUserProfile, updateUserProfile } from '../controllers/userControllers.js';
+import { addOrderItems , getOrderById} from '../controllers/orderController.js';
 import { protect } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 router.route('/').post(protect, addOrderItems)
+router.route('/:id').get(protect, getOrderById)
 
 export default router;
